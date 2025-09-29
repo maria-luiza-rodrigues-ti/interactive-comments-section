26c79dc35c34c3397eac6b14aa97b32bd8bbd6cb
Maria Luiza Rodrigues <86676797+maria-luiza-rodrigues-ti@users.noreply.github.com>
2025-09-29 16:00:09 +0000

    feat(posts): enhance getPosts route to include user data and search functionality

--- app/server/src/routes/get-posts.ts
+++ app/server/src/routes/get-posts.ts
@@ -1,7 +1,8 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { db } from "../database/client.ts";
-import { posts } from "../database/schema.ts";
+import { posts, users } from "../database/schema.ts";
+import { or, SQL, ilike, and, eq, asc } from "drizzle-orm";
 
 export const getPostsRoute: FastifyPluginAsyncZod = async (server) => {
   server.get(
@@ -10,10 +11,13 @@ export const getPostsRoute: FastifyPluginAsyncZod = async (server) => {
       schema: {
         tags: ["posts"],
         summary: "Get all posts",
-        queryString: z.object({
+        querystring: z.object({
           search: z.string().optional(),
-          orderBy: z.enum(["content", "username"]).optional(),
-          page: z.coerce.number().optional(),
+          orderBy: z
+            .enum(["content", "username"])
+            .optional()
+            .default("content"),
+          page: z.coerce.number().optional().default(1),
         }),
         response: {
           200: z.object({
@@ -23,16 +27,54 @@ export const getPostsRoute: FastifyPluginAsyncZod = async (server) => {
                 userId: z.uuid(),
                 content: z.string(),
                 createdAt: z.date().nullable(),
+                username: z.string().nullable(),
+                avatar: z.string().nullable(),
               })
             ),
+            total: z.number().nullable(),
           }),
         },
       },
     },
     async (request, reply) => {
-      const result = await db.select().from(posts);
+      const { search, page, orderBy } = request.query;
 
-      return reply.status(200).send({ posts: result });
+      const conditions: SQL[] | undefined = [];
+
+      const orCondition = or(
+        ilike(posts.content, `%${search}%`),
+        ilike(users.username, `%${search}%`)
+      );
+
+      if (search) {
+        conditions.push(orCondition!);
+      }
+
+      const [result, total] = await Promise.all([
+        db
+          .select({
+            id: posts.id,
+            userId: posts.userId,
+            content: posts.content,
+            createdAt: posts.createdAt,
+            username: users.username,
+            avatar: users.avatar,
+          })
+          .from(posts)
+          .leftJoin(users, eq(posts.userId, users.id))
+          .where(and(...conditions))
+          .orderBy(
+            orderBy === "username" ? asc(users.username) : asc(posts.content)
+          )
+          .offset((page - 1) * 10)
+          .limit(10),
+        db.$count(
+          posts,
+          search ? ilike(posts.content, `%${search}%`) : undefined
+        ),
+      ]);
+
+      return reply.status(200).send({ posts: result, total });
     }
   );
 };
