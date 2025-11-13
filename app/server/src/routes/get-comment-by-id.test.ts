69c2f6604d69ab49f4c5500650eee13437fbd6bd
Maria Luiza Rodrigues <86676797+maria-luiza-rodrigues-ti@users.noreply.github.com>
2025-11-13 21:49:19 +0000

    feat(tests): add unit test for retrieving a comment by id

---
+++ app/server/src/routes/get-comment-by-id.test.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+import { expect, test } from "vitest";
+
+import { server } from "../../app.ts";
+import { makeUser } from "../test/factories/make-user.ts";
+import { makePost } from "../test/factories/make-post.ts";
+import { makeComment } from "../test/factories/make-comment.ts";
+
+test("get a comment by id", async () => {
+  await server.ready();
+
+  const user = await makeUser();
+  const post = await makePost(user.id);
+  const comment = await makeComment({ userId: user.id, postId: post.id });
+
+  const response = await request(server.server).get(`/comments/${comment.id}`);
+
+  expect(response.status).toEqual(200);
+  expect(response.body).toEqual({
+    comment: {
+      id: comment.id,
+      postId: post.id,
+      userId: user.id,
+      content: comment.content,
+      createdAt: comment.createdAt?.toISOString() || null,
+      username: user.username,
+      avatar: user.avatar,
+      parentCommentId: null,
+      score: 0,
+    },
+  });
+});
